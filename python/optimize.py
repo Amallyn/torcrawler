311d78092642214261bc0ce953aa912d73584272
M0t13y <76857445+M0t13y@users.noreply.github.com>
2021-01-15 15:03:36 +0100

    Frontera like in place - NYT Ignore seeds in Frontier - Optimize WIP

--- python/optimize.py
+++ python/optimize.py
@@ -2,21 +2,18 @@
 
 u"""optimize
 
-Check the .xlsx file
-Parse again downloaded wwww/html/www.nytimes3xbfgragh.onion/*.html files
+Check the .xlsx file for new seeds for example
 Life Cycle: see main
 """
 
-__usage__ = u"""Usage: python3 optimize.py [options] [url]
+__usage__ = u"""Usage: python3 optimize.py [options]
 
 Options:
-  -u ..., --url=...       Website URL to crawl
   -h, --help              show this help
-  -p, --path              www path where files were stored eg. /var/www
   -d                      show debugging information
 
 Examples:
-  python3 optimize.py --url='https://www.nytimes3xbfgragh.onion/' --path='/var/www'
+  python3 optimize.py
 """
 
 __author__ = u"M0t13y"
@@ -27,9 +24,45 @@ __license__ = u"Licensed under the Apache License, Version 2.0"
 
 import os
 
+from settings import SEEDS, WWW_DIR
 from settings import WORKBOOK
 from workbook import CrawlWorkbook 
 
+class Optimize():
+    """
+    Optimize
+
+    Rebuild the crawler status aka a Seed Workbook with for example new seeds
+    """
+    crawl_book = None
+    
+    url_base = ''
+
+    # weighted links to crawl
+    weighted_links = None
+    # weighted links crawled
+    weighted_links_done = []
+    # weighted    
+    ignore_seeds = []
+    # weighted    
+    ignored_pages = []
+        
+    def __init__(self, url=next(iter(SEEDS)), path=WWW_DIR):
+        self.crawl_book = CrawlWorkbook(path=WWW_DIR, url=url)
+        self.crawl_book.wb_open()
+
+        # retrieve weighted_links, weighted_links_done...
+        self.weighted_links = self.crawl_book.weighted_links        
+        self.weighted_links_done = self.crawl_book.weighted_links_done       
+        self.ignore_seeds =  self.crawl_book.ignore_seeds
+        self.ignored_pages =  self.crawl_book.ignored_pages
+
+    def rewrite_new_seeds(self)
+        """
+        Rewrite the crawlbook based on new seeds present
+        """
+        pass
+        
 class Usage(Exception):
     def __init__(self, msg):
         self.msg = msg
@@ -44,11 +77,9 @@ def main(argv=None):
 
     try:
         try:                                
-            opts, args = getopt.getopt(argv, "hup:d", ["help", "url=", "path="])
+            opts, args = getopt.getopt(argv, "h:d", ["help"])
         except getopt.error as msg:
              raise Usage(msg)            
-        url = u'http://localhost'
-        path = u'/var/www'
         for opt, arg in opts:
             if opt in ("-h", "--help"):
                 print(__usage__)                   
@@ -56,29 +87,20 @@ def main(argv=None):
             elif opt == '-d':
                 global _debug               
                 _debug = 1                  
-            elif opt in ("-u", "--url"):
-                url = arg               
-            elif opt in ("-p", "--path"):
-                path = arg               
     except Usage as err:
         print >>sys.stderr, err.msg
         print >>sys.stderr, "for help use --help"
         return 2
 
-    if len(argv)<2:
-        print("Arguments error")
-        print(__usage__)
-        sys.exit(2)
+#    if len(argv)<2:
+#        print("Arguments error")
+#        print(__usage__)
+#        sys.exit(2)
     
     # Get the list of all files and directories 
     # in the root directory
-    cw = CrawlWorkbook(path, url)
-    cw.wb_open() 
-    
-    print(len(os.listdir(cw.html_pages_path)), 'files in Html Pages path:', cw.html_pages_path)
-    # print the list 
-    # print( os.listdir(cw.html_pages_path)) 
-    
+    opti = Optimize()
+        
 if __name__ == "__main__":
     import sys
     print("\n" + __doc__ + "\n" + __copyright__ + "\n" + __license__ +"\n" )
